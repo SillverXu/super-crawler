c90313d5f65707d67f77b633091b02dbe6e0beb8
SillverXu <31513030+SillverXu@users.noreply.github.com>
2017-12-25 14:15:00 +0800

    add requestHeader default pages change to -1

--- src/main/java/com/bmsoft/xu/bean/TaskBean.java
+++ src/main/java/com/bmsoft/xu/bean/TaskBean.java
@@ -11,11 +11,11 @@ import static com.bmsoft.xu.utils.Var.HTTPGET;
 public class TaskBean {
 
     private int requesttype = HTTPGET;  //默认为HTTPGET
-    private Map<String,String> requestParams = null;       //请求所带参数
-    private Map<String,String> requestHeaders = null;
+    private Map<String, String> requestParams = null;       //请求所带参数
+    private Map<String, String> requestHeaders = null;
     private String url_start = null;       //url 前部
     private String url_end = null;         //url 后部
-    private int pages = 0;              //翻页操作的页数
+    private int pages = -1;              //翻页操作的页数
     private CrawlerBean crawlerBean = null;    //爬取页面策略
     private List<RuleBean> ruleList = null;   //爬取首页策略
     private boolean isPinjie = false;
@@ -24,9 +24,11 @@ public class TaskBean {
     public TaskBean() {
     }
 
-    public TaskBean( String url_start, String url_end,int requesttype, Map requestParams, int pages, CrawlerBean crawlerBean, boolean isPinjie, String pinjieStr,List<RuleBean> ruleList) {
+    public TaskBean(String url_start, String url_end, int requesttype, Map requestParams, int pages,
+                    Map<String,String> requestHeaders,CrawlerBean crawlerBean, boolean isPinjie, String pinjieStr, List<RuleBean> ruleList) {
         this.requesttype = requesttype;
         this.requestParams = requestParams;
+        this.requestHeaders = requestHeaders;
         this.url_start = url_start;
         this.url_end = url_end;
         this.pages = pages;
@@ -51,11 +53,11 @@ public class TaskBean {
         this.requesttype = requesttype;
     }
 
-    public Map<String,String> getRequestParams() {
+    public Map<String, String> getRequestParams() {
         return requestParams;
     }
 
-    public void setRequestParams(Map<String,String> requestParams) {
+    public void setRequestParams(Map<String, String> requestParams) {
         this.requestParams = requestParams;
     }
 
